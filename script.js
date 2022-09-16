8c486a88c02db72df65146b9e919912fed633416
Usenmfon <usenmfonuko@gmail.com>
2022-09-16 05:44:07 +0100

    mobile responsiveness

--- script.js
+++ script.js
@@ -3,7 +3,6 @@ const table_body = document.getElementsByTagName('tbody')[0];
 const section = document.getElementsByTagName('section')[0];
 const recent_button = document.getElementsByTagName('button')[0];
 
-
 let arr = []
 let output = []
 
@@ -18,9 +17,12 @@ formEle.addEventListener('submit', function(e){
     arr = entries.text.split("\n")
     
     for(let i = 0; i < arr.length; i++){
+        let extract_item = arr[i].slice(0, arr[i].indexOf(arr[i].match(/(\d+)/)[0]));
+        let extract_price = Number(arr[i].match(/(\d+)/)[0]);
+
         output.push({
-            text : arr[i].slice(0, arr[i].indexOf(arr[i].match(/(\d+)/)[0])),
-            value: Number(arr[i].match(/(\d+)/)[0]),
+            item : extract_item,
+            price: extract_price,
         })
     }
 
@@ -30,10 +32,10 @@ formEle.addEventListener('submit', function(e){
     formEle.reset();
 })
 
-function totalSum(data){
+function totalSum(obj){
     let sum = 0;
-    for(let num in data){
-        sum += data[num].value
+    for(let key in obj){
+        sum += obj[key].price
     }
    return sum.toString().replace(/\B(?<!\.\d*)(?=(\d{3})+(?!\d))/g, ",");
 }
@@ -45,15 +47,15 @@ function displayResult(data){
     if(data){
         section.style.display = "block";
     }
-    data.push({ text: "Total", value: totalSum(data) })
+    data.push({ item: "Total", price: totalSum(data) })
 
     for(let i in data){
         tr = document.createElement('tr');
         td_1 = document.createElement('td');
         td_2 = document.createElement('td');
 
-        td_1.innerText = data[i].text
-        td_2.innerText = data[i].value
+        td_1.innerText = data[i].item
+        td_2.innerText = data[i].price
 
         tr.append(td_1, td_2)
         table_body.appendChild(tr)
